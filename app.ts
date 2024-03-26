3b63e9f688dd6d762c1a9627640079295b9bba8a
bushra <161301232+bushkhan123@users.noreply.github.com>
2024-03-26 08:07:06 -0700

    2.personal message

--- app.ts
+++ app.ts
@@ -1,3 +1,10 @@
-let message = "Hello world"
+let personName :string = '';
 
-console.log("message");
+personName = prompt('what is your name?') || '';
+
+if(personName !== null && personName !== ''){
+    alert(`Hello $(peNamerson), would you like to learn some Python today?`)
+}
+else{
+    alert('you have to fill your name !')
+}
